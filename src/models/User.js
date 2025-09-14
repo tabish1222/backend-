3a626982099317a412fc998cb33672c525d5852d
tabish1222 <107319072+tabish1222@users.noreply.github.com>
2025-09-14 23:45:47 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,11 @@
-const { DataTypes } = require("sequelize");
-const { sequelize } = require("../db");
+// src/models/User.js
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db.js";
 
-const Student = sequelize.define("Student", {
+export const User = sequelize.define("User", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   name: { type: DataTypes.STRING, allowNull: false },
-  class: { type: DataTypes.STRING, allowNull: false },
-  age: { type: DataTypes.INTEGER, allowNull: false },
-  parentId: { type: DataTypes.INTEGER, allowNull: false },
+  email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.ENUM("parent", "teacher"), allowNull: false },
 });
-
-module.exports = Student;
