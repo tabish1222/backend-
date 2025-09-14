c77159a4a13d5a21fd6542c464dda0edc823409b
tabish1222 <107319072+tabish1222@users.noreply.github.com>
2025-09-14 18:42:04 +0530

    Update Student.js

--- src/models/Student.js
+++ src/models/Student.js
@@ -1,15 +1,13 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/postgres");
-const User = require("./User");
+const mongoose = require("mongoose");
 
-const Student = sequelize.define("Student", {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: DataTypes.STRING, allowNull: false },
-  age: { type: DataTypes.INTEGER, allowNull: false }
-});
+const StudentSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    grade: { type: String, required: true },
+    parent: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+  },
+  { timestamps: true }
+);
 
-// A parent has many children
-User.hasMany(Student, { foreignKey: "parentId", as: "children" });
-Student.belongsTo(User, { foreignKey: "parentId", as: "parent" });
-
-module.exports = Student;
+module.exports = mongoose.model("Student", StudentSchema);
