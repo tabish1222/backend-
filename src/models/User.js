b65791a4507dc05da13f7cfe9fe6f66ae22c0b64
tabish1222 <107319072+tabish1222@users.noreply.github.com>
2025-09-14 20:59:47 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../db");
+const { sequelize } = require("../db"); // ✅ import the ORM instance
 
 const User = sequelize.define("User", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
