ca734e577d8a233fb6ad572958568535536b70e8
tabish1222 <107319072+tabish1222@users.noreply.github.com>
2025-09-17 15:59:18 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,24 @@
-// src/models/index.js
 import { User } from "./User.js";
 import { Student } from "./Student.js";
+import { Subject } from "./Subject.js";
+import { StudentTeacher } from "./StudentTeacher.js";
 
-// Define associations
+// Parent ↔ Student
 User.hasMany(Student, { foreignKey: "parent_id", as: "children" });
 Student.belongsTo(User, { foreignKey: "parent_id", as: "parent" });
 
-export { User, Student };
+// Teacher ↔ Student (many-to-many through StudentTeacher)
+User.belongsToMany(Student, {
+  through: StudentTeacher,
+  foreignKey: "teacher_id",
+  as: "students",
+});
+Student.belongsToMany(User, {
+  through: StudentTeacher,
+  foreignKey: "student_id",
+  as: "teachers",
+});
+
+// TeacherSubject ↔ Subject
+Subject.hasMany(StudentTeacher, { foreignKey: "subject_id" });
+StudentTeacher.belongsTo(Subject, { foreignKey: "subject_id" });
